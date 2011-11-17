4853ef5d7a64a35bfcc523a567a935f534db1c77
Ronaldo Amaral <rsantos@iff.edu.br>
2011-11-17 19:58:21 -0200

    corrigindo js de calculo de valor total

--- site_media/js/ordem_servico.js
+++ site_media/js/ordem_servico.js
@@ -1,16 +1,18 @@
 var $ = django.jQuery;
 
 $(document).ready(function() {
-  $("td.valor>input").bind("focus", function() {
-    $("td.valor>input").each(function() {
-        $(this).blur(function() {
-            var total=0;
-            $("td.valor>input").each(function() {
-                total += Number($(this).val());
-            });
-            $("#id_valor_total").val(total);
+  
+  var soma_total =  function() {
+        var total=0;
+        $("td.valor>input").each(function() {
+            total += Number($(this).val());
         });
-    });
-  });  
+        $("#id_valor_total").val(total);
+  }; 
+ 
+  $("td.valor>input").live("blur", soma_total ); 
+  $('tbody > tr[id*=itemservico]').parent('tbody').bind("DOMSubtreeModified", soma_total )
 })
+
+
 
